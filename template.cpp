36ae38f0f7b8cf013d2ea116cb6de51e4ff4b8cf
yonaMac <yonaimyo@gmail.com>
2023-03-21 23:19:39 +0900

    template.cpp 추가

--- template.cpp
+++ template.cpp
@@ -3,11 +3,15 @@
 */
 
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 #include <math.h>
 #include <limits.h>
 
 #define max(a,b)  (((a) > (b)) ? (a) : (b))
 #define min(a,b)  (((a) < (b)) ? (a) : (b))
+
+const int MaxN = 100000;
 
 int main()
 {
